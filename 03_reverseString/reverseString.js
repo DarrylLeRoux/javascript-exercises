5398fa0a5f1aedfda8b820b8f7a9f64e8d1c53bf
Darryl Le Roux <devdarryl@outlook.com>
2022-05-13 17:54:17 +0200

    Reversed a string test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  const strSplit = str.split("");
+  strSplit.reverse();
+  return strSplit.join("");
 };
 
 // Do not edit below this line
